ec2a683665a9bd83063e21885fbbf34757b1d694
Keks <keks@htmlacademy.ru>
2021-03-09 08:23:58 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/search.js
@@ -0,0 +1,42 @@
+const formOpenButton = document.querySelector(".form-open");
+const searchForm = document.querySelector(".search-form");
+const checkInDate = searchForm.querySelector("[name=check-in-date]");
+const checkOutDate = searchForm.querySelector("[name=check-out-date]");
+const adults = searchForm.querySelector("[name=number-of-adults]");
+const kids = searchForm.querySelector("[name=number-of-kids]");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storageKids = localStorage.getItem("kids");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+try {
+  storageAdults = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+formOpenButton.addEventListener("click", function () {
+  if (searchForm.classList.contains("search-form-hide")) {
+    searchForm.classList.remove("search-form-hide");
+    searchForm.classList.add("search-form-open");
+    checkInDate.focus();
+    if (isStorageSupport) {
+      adults.value = storageAdults
+      kids.value = storageKids
+    }
+  } else {
+    searchForm.classList.remove("search-form-open");
+    searchForm.classList.add("search-form-hide")
+  }
+});
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!checkInDate.value || !checkOutDate.value || !adults.value || !kids.value) {
+    evt.preventDefault();
+  }
+});
